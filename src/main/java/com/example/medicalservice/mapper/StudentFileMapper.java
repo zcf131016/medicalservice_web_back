dff7b8c38e7e7b83722bd44bf68b3265b993572e
Liam <lyhdxzb@icloud.com>
2021-06-23 09:01:23 +0800

    修改部分接口，增加学生文件接口

---
+++ src/main/java/com/example/medicalservice/mapper/StudentFileMapper.java
@@ -0,0 +1,16 @@
+package com.example.medicalservice.mapper;
+
+import com.example.medicalservice.domain.StudentFile;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Lin YuHang
+ * @date 2021/6/22 17:02
+ */
+@Mapper
+public interface StudentFileMapper {
+    List<StudentFile> getFileByStudentId(Integer caseId,Integer studentId);
+    void addFile(StudentFile studentFile);
+}
