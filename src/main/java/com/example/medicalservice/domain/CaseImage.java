f62741cf50dab28d9b4ad9140ee5f1355ae65170
zx7450 <13405969241@qq.com>
2021-06-27 20:55:39 +0800

    修改了查看图片的返回结果以及批量删除案例的传参方法

--- src/main/java/com/example/medicalservice/domain/CaseImage.java
+++ src/main/java/com/example/medicalservice/domain/CaseImage.java
@@ -1,56 +1,22 @@
 package com.example.medicalservice.domain;
 
 import io.swagger.annotations.ApiModel;
+import jdk.jfr.DataAmount;
+import lombok.Data;
 
+import java.sql.Date;
 import java.util.Arrays;
-import java.util.Date;
+
 @ApiModel
+@Data
 public class CaseImage {
     private Integer id;
     private Integer caseId;
     private Object image;
     private Date creatTime;
-
-    @Override
-    public String toString() {
-        return "CaseImage{" +
-                "id=" + id +
-                ", caseId=" + caseId +
-                ", image=" + image +
-                ", creatTime=" + creatTime +
-                '}';
-    }
-
-    public Object getImage() {
-        return image;
-    }
-
-    public void setImage(Object image) {
-        this.image = image;
-    }
-
-    public Date getCreatTime() {
-        return creatTime;
-    }
-
-    public void setCreatTime(Date creatTime) {
-        this.creatTime = creatTime;
-    }
+    private String imageName;
 
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
-    public Integer getCaseId() {
-        return caseId;
-    }
-
-    public void setCaseId(Integer caseId) {
-        this.caseId = caseId;
-    }
 }
