4db815ee9146f47cdc9c8f2c376b9f0e7224d956
Liam <lyhdxzb@icloud.com>
2021-07-06 14:18:00 +0800

    修改sql文件

--- sql/case_image.sql
+++ sql/case_image.sql
@@ -1,17 +1,17 @@
 /*
- Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
- Source Server         : test
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80023
+ Source Server Version : 50719
  Source Host           : localhost:3306
  Source Schema         : medicalservice
 
  Target Server Type    : MySQL
- Target Server Version : 80023
+ Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 27/06/2021 20:59:37
+ Date: 06/07/2021 08:46:28
 */
 
 SET NAMES utf8mb4;
@@ -22,12 +22,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `case_image`;
 CREATE TABLE `case_image`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `case_id` int NULL DEFAULT NULL COMMENT '案例id',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `case_id` int(11) NULL DEFAULT NULL COMMENT '案例id',
   `image` longblob NULL COMMENT '图片资源',
   `image_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `creat_time` datetime(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_case_id`(`case_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
